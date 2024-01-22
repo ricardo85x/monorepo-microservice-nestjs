750b34bd8572124b7b47d606660c022b875a9378
Ricardo <ricardo85x@gmai.com>
2024-01-22 08:04:25 -0300

    fix: Joi import

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
