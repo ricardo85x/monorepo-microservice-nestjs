4ab43361c56b2ace93d61e4adc64f7403fcd7430
Ricardo <ricardo85x@gmai.com>
2024-01-22 07:18:45 -0300

    feat: add import to config module

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [NestConfigModule.forRoot()],
+})
 export class ConfigModule {}
