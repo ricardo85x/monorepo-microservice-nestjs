cd60ce1b0e6dfb4aacfad0ff4d30371222b67b33
Ricardo <ricardo85x@gmai.com>
2024-01-22 08:17:16 -0300

    feat: generate reservations resource
    command used: nest g resource reservations

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
 import { DatabaseModule } from '@app/common';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ReservationsModule],
   controllers: [ReservationsController],
   providers: [ReservationsService],
 })
