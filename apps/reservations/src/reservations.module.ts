ec0266504922d1e34073ffe6149f3df5e611af36
Ricardo <ricardo85x@gmai.com>
2024-01-22 08:19:42 -0300

    refactor: move the reservation resource to reservation/src

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
-import { DatabaseModule } from '@app/common';
-import { ReservationsModule } from './reservations/reservations.module';
+import { ReservationsController } from './reservations.controller';
 
 @Module({
-  imports: [DatabaseModule, ReservationsModule],
   controllers: [ReservationsController],
   providers: [ReservationsService],
 })
