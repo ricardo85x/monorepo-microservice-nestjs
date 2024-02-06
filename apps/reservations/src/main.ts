83bb2c9e07cceb055aaa7e9102d3d8fc465f7181
Ricardo <ricardo85x@gmai.com>
2024-02-06 07:54:32 -0300

    feat: add logger module

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -3,6 +3,7 @@ import { ReservationsModule } from './reservations.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
